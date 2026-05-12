0930f4dbfae701b8f887be7e63a6ed4863497a97
fearlessfreakk <23052229@kiit.ac.in>
2026-05-12 16:28:23 +0530

    feat: implement sidebar layout with organization switching and creation capabilities

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -1,29 +1,43 @@
-"use client"
+"use client";
 
 import { Plus } from "lucide-react";
 import { CreateOrganization } from "@clerk/nextjs";
+import { Hint } from "@/components/ui/hint";
 
-import{
-    Dialog,
-    DialogContent,
-    DialogTrigger,
-    DialogTitle,
-}from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogTitle,
+  DialogDescription,
+  DialogHeader,
+} from "@/components/ui/dialog";
 
 export const Newbutton = () => {
-    return(
-        <Dialog>
-            <DialogTrigger asChild>
-                <div className="aspect-square">
-                    <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
-                        <Plus className="text-white" />
-                    </button>
-                </div>
-            </DialogTrigger>
-            <DialogContent className="p-0 bg-transparent border-none max-w-[430px]">
-                <DialogTitle className="sr-only">Create organization</DialogTitle>
-                <CreateOrganization />
-            </DialogContent>
-        </Dialog>
-    )
-}
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="aspect-square">
+          <Hint
+            side="right"
+            align="start"
+            sideOffset={18}
+            alignOffset={0}
+            label="Create an organization"
+          >
+            <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+              <Plus className="text-white" />
+            </button>
+          </Hint>
+        </div>
+      </DialogTrigger>
+      <DialogContent
+        className="p-0 bg-transparent border-none max-w-[430px] shadow-none ring-0"
+        showCloseButton={false}
+      >
+        <DialogTitle className="sr-only">Create organization</DialogTitle>
+        <CreateOrganization />
+      </DialogContent>
+    </Dialog>
+  );
+};
